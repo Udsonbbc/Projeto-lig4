599f1a10fa878f1e433831b8934e24c3bdcee0cc
Udson <udsonbbc@hotmail.com>
2020-09-22 03:29:28 -0300

    Criando estrutura Html e evento de clique inicial

---
+++ script.js
@@ -0,0 +1,8 @@
+//Evento de clique que irá iniciar o jogo, mostrando ao usuário os dois discos "Preto/Vermelho"
+const disco = document.getElementById("disco");
+const disco2 = document.getElementById("disco2");
+const vermelho = document.getElementById("button");
+button.onclick = function() {
+     disco.className = "vermelho";
+     disco2.className = "preto";
+    };
