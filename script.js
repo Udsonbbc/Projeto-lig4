4ec327ba6081d194ecae34007fd72c92e3e2ecf0
Fernando Santos <fernandosantos511@yahoo.com.br>
2020-09-23 13:14:32 -0300

    Começo das condições de vitória

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 let disco = document.getElementById("disco");
 const disco2 = document.getElementById("disco2");
 const vermelho = document.getElementById("button");
-button.addEventListener('click', () => {
-     disco.className = "vermelho";
-     disco2.className = "preto";
-});
+button.addEventListener('click', setGame);
 
 
 //função dque adiciona os discos ma tabela, temos que estudar uma maneira de colocar os disco no começo da tabela, além disso é necessário criar uma
@@ -18,47 +15,47 @@ document.body.addEventListener("dblclick", (e) => {
 })
 
 //F: Adicionei uma classe para todos os quadrados da tabela e para elas distribui a função de adicionar a peça do jogo
+function setGame() {
 
-let funcaoColuna = document.querySelectorAll('.coluna');
-for (let i = 0; i < funcaoColuna.length; i++) {
-     funcaoColuna[i].addEventListener('click', addDisco);
-}
-let isBlack = false //F: Esta variável faz com que decida qual peça está jogando, e ao final do turno troca de jogador
-//F: A função pega a primeira classe do quadrado clicado, que representa a sua coluna e verifica a partir da linha se já há
-//algumas "peça" nela (hasChildNodes), caso não tenha ela insere o disco, troca de jogador e termina a função
-//caso contrário, se tiver, ela verifica a próxima linha
-function addDisco() {
-     let parentClass = this.className
-     let firstClass = parentClass.split(' ')[0]
-     console.log(firstClass)
-     if (!isBlack) {
-          for (let linha = 1; linha <= 7; linha++) {
-               if (document.querySelector(`#${firstClass}L${linha}`).hasChildNodes()) {
-                    console.log('não')
-                    continue
-               } else {
-                    console.log('sim')
-                    let discoVermelho = document.createElement('div');
-                    discoVermelho.className = 'vermelho'
-                    document.querySelector(`#${firstClass}L${linha}`).appendChild(discoVermelho);
-                    isBlack = true
-                    break
+     let funcaoColuna = document.querySelectorAll('.coluna');
+     for (let i = 0; i < funcaoColuna.length; i++) {
+          funcaoColuna[i].addEventListener('click', addDisco);
+     }
+     let isBlack = false //F: Esta variável faz com que decida qual peça está jogando, e ao final do turno troca de jogador
+     //F: A função pega a primeira classe do quadrado clicado, que representa a sua coluna e verifica a partir da linha se já há
+     //algumas "peça" nela (hasChildNodes), caso não tenha ela insere o disco, troca de jogador e termina a função
+     //caso contrário, se tiver, ela verifica a próxima linha
+     function addDisco() {
+          let parentClass = this.className
+          let firstClass = parentClass.split(' ')[0]
+          if (!isBlack) {
+               for (let linha = 1; linha <= 6; linha++) {
+                    if (document.querySelector(`#${firstClass}L${linha}`).hasChildNodes()) {
+                         continue
+                    } else {
+                         let discoVermelho = document.createElement('div');
+                         discoVermelho.className = 'vermelho'
+                         document.querySelector(`#${firstClass}L${linha}`).appendChild(discoVermelho);
+                         isBlack = true
+                         break
+                    }
                }
-          }
-     } else {
-          for (let linha = 1; linha <= 7; linha++) {
-               if (document.querySelector(`#${firstClass}L${linha}`).hasChildNodes()) {
-                    console.log('não')
-                    continue
-               } else {
-                    console.log('sim')
-                    let discoPreto = document.createElement('div');
-                    discoPreto.className = 'preto'
-                    document.querySelector(`#${firstClass}L${linha}`).appendChild(discoPreto);
-                    isBlack = false
-                    break
+          } else {
+               for (let linha = 1; linha <= 6; linha++) {
+                    if (document.querySelector(`#${firstClass}L${linha}`).hasChildNodes()) {
+                         continue
+                    } else {
+                         let discoPreto = document.createElement('div');
+                         discoPreto.className = 'preto'
+                         document.querySelector(`#${firstClass}L${linha}`).appendChild(discoPreto);
+                         isBlack = false
+                         break
+                    }
                }
           }
      }
 }
+
+let linhas = document.querySelectorAll('tr');
+console.log(linhas.length)
 
